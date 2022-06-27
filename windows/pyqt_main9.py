ecc2933e6c73af029f6311379d2e7dd2b4bab88f
YongJunPark <june3004@naver.com>
2022-06-27 13:53:33 +0900

    1111

--- windows/pyqt_main9.py
+++ windows/pyqt_main9.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     wnd = MyApp()
     app.exec()
-# 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
+# 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
